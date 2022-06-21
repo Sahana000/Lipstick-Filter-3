26d94e49a7aab92857d480506cb9bcee231e53d0
Sahana000 <91887252+Sahana000@users.noreply.github.com>
2022-06-21 17:46:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/L5DQZsWQ/lip-clipart-8.png');
+}
+
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x -25;
+        noseY = results[0].pose.nose.y+15;
+        console.log("nose x ="+ noseX);
+        console.log("nose y ="+ noseY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 50, 20);
+}
+
+function take_snapshoot() {
+    save('myFilterImage.png');
+}
